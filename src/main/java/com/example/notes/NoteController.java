4d9e8d7e4a43a1fa9e0b84fffc705ac54a2f39ae
Parikshit Narang <parikshitnarang@Parikshits-MacBook-Pro.local>
2024-01-03 07:22:59 -0500

    ACT-removed unused imports from NoteController

--- src/main/java/com/example/notes/NoteController.java
+++ src/main/java/com/example/notes/NoteController.java
@@ -1,13 +1,10 @@
 package com.example.notes;
 
-import com.example.user.LoginRequest;
-import com.example.user.UserController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
