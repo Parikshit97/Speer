983b8cebe803c81a08af3b657eb0367f348acb4c
Parikshit Narang <parikshitnarang@Parikshits-MacBook-Pro.local>
2024-01-03 11:12:03 -0500

    EDT-implemented update existing note feature for authenticated user

---
+++ src/main/java/com/example/notes/UpdateNote.java
@@ -0,0 +1,15 @@
+package com.example.notes;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateNote {
+    private String title;
+    private String content;
+}
